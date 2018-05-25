0e7c1ab9e5aff860a593ed5956a8286b167df0cd
Trevor Spear <speart@oregonstate.edu>
2018-05-24 19:26:43 -0700

    Adding delete request and some fixes to the other request.

--- smash-bros.js
+++ smash-bros.js
@@ -177,7 +177,9 @@ app.post('/roster',function(req,res,next){
 //Edit a roster
 //CHECK THAT THE PERSON DOING THE EDIT IS THE SAME AS THE ONE THAT CREATED IT
 //Not Done - Page Not Done
-app.put('/roster/:id',function(req,res,next){
+app.put('/roster/:rosterID',function(req,res,next){
+    const rosterID = parseInt(req.params.rosterID);
+
     if(req.body && req.body.user && req.body.id){
 
         //id (auto_increment)
@@ -202,6 +204,38 @@ app.put('/roster/:id',function(req,res,next){
 
 
 
+//Delete a roster
+//Check the one who is deleting is the same user
+//Not Done - Page Not Done
+app.delete('/roster/:rosterID',function(req,res,next){
+    const rosterID = parseInt(req.params.rosterID);
+
+    if(req.body && req.body.user){
+
+        //id (auto_increment)
+        //user (userID) (check if this user exist)
+        //rating (default this to 0)
+        //number_of_ratings (default this to 0)
+
+        //Check if user exist
+        mysql.pool.query('SELECT * FROM sbr_users WHERE id=' + req.body.user + "", function(err, rows, fields){
+            if(!err) {
+
+                //Check if user is same as the one that is editing it.
+                //Do something here
+
+                mysql.pool.query('DELETE FROM `sbr_roster` WHERE `id` = ' + rosterID + '', function (err, rows, fields) {
+                    //redirect here
+
+                });
+            }
+        });
+    }
+});
+
+
+
+
 //Get characters
 //Need to grab all the rosters and anything that contains roster information here
 //Done - Page Not Done
@@ -213,7 +247,7 @@ app.get('/character',function(req,res,next){
             next()
 
         }else {
-            context.sbr_roster = rows;
+            context.sbr_character = rows;
 
             mysql.pool.query('SELECT * FROM sbr_character_comment', function(err, rows, fields){
 
@@ -222,7 +256,7 @@ app.get('/character',function(req,res,next){
 
                 }else {
 
-                    context.sbr_roster_comment = rows;
+                    context.sbr_character_comment = rows;
                     res.render('character', context);
 
                 }
@@ -235,8 +269,7 @@ app.get('/character',function(req,res,next){
 
 
 
-//Create a character
-//Need to validate that user exist.... BUT THE FORM SHOULD JUST HAVE VALID USERS IN IT SO IDK
+
 //Not Done - Page Not Done
 app.post('/character',function(req,res,next){
     if(req.body && req.body.user && req.body.name && req.body.picture && req.body.description && req.body.moves){
@@ -267,10 +300,13 @@ app.post('/character',function(req,res,next){
 
 
 
-//Edit a roster
-//CHECK THAT THE PERSON DOING THE EDIT IS THE SAME AS THE ONE THAT CREATED IT
 //Not Done - Page Not Done
-app.put('/character',function(req,res,next){
+//Validate
+//Redirect
+//CHECK THAT THE PERSON DOING THE EDIT IS THE SAME AS THE ONE THAT CREATED IT
+app.put('/character/:characterID',function(req,res,next){
+    const characterID = parseInt(req.params.characterID);
+
     if(req.body && req.body.user && req.body.name && req.body.picture && req.body.description && req.body.moves){
 
         //id (auto_increment)                                                   -- Don't change
@@ -295,26 +331,71 @@ app.put('/character',function(req,res,next){
 
 
 
+//Not Done
+//Validate
+//Redirect
+//CHECK THAT THE PERSON DOING THE EDIT IS THE SAME AS THE ONE THAT CREATED IT
+app.delete('/character/:characterID',function(req,res,next){
+    const characterID = parseInt(req.params.characterID);
 
-//Make a user
+    if(req.body && req.body.user){
+
+        //Check if user exist
+        mysql.pool.query('SELECT * FROM sbr_users WHERE id=' + req.body.user + "", function(err, rows, fields) {
+            if (!err) {
+
+                //Check if user is same as the one that is own it
+                //Do something here
+
+                mysql.pool.query('DELETE FROM `sbr_roster` WHERE `id` = ' + rosterID + '', function (err, rows, fields) {
+                    //redirect here
+
+                });
+
+            }
+        });
+
+    }
+});
+
+
+
+
+
+//Done
+app.get('/user',function(req,res,next){
+    var context = {};
+
+    mysql.pool.query('SELECT * FROM sbr_users WHERE id=' + req.body.user + '', function(err, rows, fields){
+        context.sbr_users = rows;
+        res.render('users', context);
+    });
+
+});
+
+
+
+//Not Done
+//Validate
+//Redirect
 app.post('/user',function(req,res,next){
     if(req.body && req.body.username && req.body.email && req.body.first_name && req.body.last_name && req.body.password){
 
         //id (auto_increment)
-        //user (userID) (check if this user exist)
-        //name string
-        //picture (unknown) -------------------------------------------------
-        //description (text)
-        //moves (text)
-        //rating (default this to 0)
-        //number_of_rating (default this to 0)
+        //username
+        //email
+        //first_name
+        //last_name
+        //password_hash
 
         //Check if user exist! ----------------------------------------------------
-        mysql.pool.query('SELECT * FROM users WHERE id=' + req.body.user + '', function(err, rows, fields){
+        mysql.pool.query('SELECT * FROM sbr_users WHERE id=' + req.body.user + '', function(err, rows, fields){
             if(!err) {
 
+                //Hash password here
+
                 //Insert Data
-                mysql.pool.query('INSERT INTO sbr_character (user, name, picture, description, moves, rating, number_of_rating) VALUES (' + req.body.user + "," + req.body.name + "," + req.body.picture + "," + req.body.description + "," + req.body.moves + ', 0, 0)', function (err, rows, fields) {
+                mysql.pool.query('INSERT INTO sbr_users (username, email, first_name, last_name, password_hash) VALUES (' + req.body.username + "," + req.body.email + "," + req.body.first_name + "," + req.body.last_name + "," + req.body.password + ')', function (err, rows, fields) {
                     //redirect here --------------------
                 });
 
@@ -324,6 +405,62 @@ app.post('/user',function(req,res,next){
     }
 });
 
+
+
+//Not Done
+//Need to hash the password
+app.put('/user/:userID',function(req,res,next){
+    const userID = parseInt(req.params.userID);
+
+    if(req.body && req.body.username && req.body.email && req.body.first_name && req.body.last_name && req.body.password){
+
+        //id (auto_increment)
+        //username
+        //email
+        //first_name
+        //last_name
+        //password_hash
+
+        //Check if user exist! ----------------------------------------------------
+        mysql.pool.query('SELECT * FROM sbr_users WHERE id=' + req.body.user + '', function(err, rows, fields){
+            if(!err) {
+
+                //Hash password here
+
+                //Insert Data
+                mysql.pool.query('UPDATE sbr_users (username, email, first_name, last_name, password_hash) VALUES (' + req.body.username + "," + req.body.email + "," + req.body.first_name + "," + req.body.last_name + "," + req.body.password + ')', function (err, rows, fields) {
+                    //redirect here --------------------
+                });
+
+
+            }
+        });
+    }
+});
+
+
+
+//Not Done
+//Validate
+//Redirect
+app.delete('/user/:userID',function(req,res,next){
+    const userID = parseInt(req.params.userID);
+
+    if(req.body && req.body.user){
+
+        mysql.pool.query('SELECT * FROM sbr_users WHERE id=' + req.body.user + "", function(err, rows, fields) {
+            if (!err) {
+
+                mysql.pool.query('DELETE FROM `sbr_user` WHERE `id` = ' + userID + '', function (err, rows, fields) {
+                    //redirect here
+
+                });
+
+            }
+        });
+
+    }
+});
 
 
 
