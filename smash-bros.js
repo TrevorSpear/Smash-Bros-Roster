5c1c79740b26330bf6f2d5f3394ddb5c110e0e2a
Trevor Spear <speart@oregonstate.edu>
2018-05-24 17:00:52 -0700

    Changes with the sql and tables

--- smash-bros.js
+++ smash-bros.js
@@ -26,54 +26,78 @@ app.get('/',function(req,res,next){
     var context = {};
 
     mysql.pool.query('SELECT * FROM sbr_users', function(err, rows, fields){
+        
         if(err){
             next()
+
         }else {
+
             context.sbr_users = rows;
+
+            mysql.pool.query('SELECT * FROM sbr_roster', function(err, rows, fields){
+
+                if(err){
+                    next()
+
+                }else {
+
+                    context.sbr_roster = rows;
+
+                    mysql.pool.query('SELECT * FROM sbr_roster_comment', function(err, rows, fields){
+
+                        if(err){
+                            next()
+
+                        }else {
+                            context.sbr_roster_comment = rows;
+
+
+                            mysql.pool.query('SELECT * FROM sbr_roster_character', function(err, rows, fields){
+
+                                if(err){
+                                    next()
+
+                                }else {
+
+                                    context.sbr_roster_character = rows;
+
+                                    mysql.pool.query('SELECT * FROM sbr_character', function(err, rows, fields){
+
+                                        if(err){
+                                            next()
+
+                                        }else {
+
+                                            context.sbr_character = rows;
+
+                                            mysql.pool.query('SELECT * FROM sbr_character_comment', function(err, rows, fields){
+
+                                                if(err){
+                                                    next()
+                                                }else {
+
+                                                    context.sbr_character_comment = rows;
+                                                    res.render('home', context);
+
+                                                }
+                                            });
+
+                                        }
+                                    });
+
+                                }
+                            });
+
+
+                        }
+                    });
+
+                }
+            });
+
         }
     });
 
-    mysql.pool.query('SELECT * FROM sbr_roster', function(err, rows, fields){
-        if(err){
-            next()
-        }else {
-            context.sbr_roster = rows;
-        }
-    });
-
-    mysql.pool.query('SELECT * FROM sbr_roster_comment', function(err, rows, fields){
-        if(err){
-            next()
-        }else {
-            context.sbr_roster_comment = rows;
-        }
-    });
-
-    mysql.pool.query('SELECT * FROM sbr_roster_character', function(err, rows, fields){
-        if(err){
-            next()
-        }else {
-            context.sbr_roster_character = rows;
-        }
-    });
-
-    mysql.pool.query('SELECT * FROM sbr_character', function(err, rows, fields){
-        if(err){
-            next()
-        }else {
-            context.sbr_character = rows;
-        }
-    });
-
-    mysql.pool.query('SELECT * FROM sbr_character_comment', function(err, rows, fields){
-        if(err){
-            next()
-        }else {
-            context.sbr_character_comment = rows;
-        }
-    });
-
-    res.render('home', context);
 });
 
 
